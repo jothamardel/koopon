70d7f122cd615d9439e4ecefdac214b91d8d5870
unknown <spontaneousmanji@gmail.com>
2022-07-22 16:21:26 +0100

    chore: creating create koopon function

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { get_greeting } = require("../controllers");
+const { createKoopon } = require("../controllers/koopon.controller");
 
 const Routes = express.Router();
 
 Routes.get("/", get_greeting);
+Routes.post("/koopon", createKoopon);
 module.exports = Routes;
